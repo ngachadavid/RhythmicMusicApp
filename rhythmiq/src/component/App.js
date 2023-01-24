95fa9e7f7dfb21738d475b88076c5d25c02ee1a5
Elizabeth <mwendemuthusi6@gmail.com>
2023-01-24 18:38:38 +0300

    Added the fetch request in the App.js

--- rhythmiq/src/component/App.js
+++ rhythmiq/src/component/App.js
@@ -1,6 +1,13 @@
+import { useEffect, useState } from "react";
 
 
 function App() {
+const [songs, setSongs]=useState([])
+  useEffect(()=>{
+    fetch('http://localhost:3000/song').then(res=>res.json())
+    .then(songs=>setSongs(songs))
+  }, [])
+  console.log(songs)
   return (
     <div className="App">
       <h1>Hey guys, ebu tuanze</h1>
