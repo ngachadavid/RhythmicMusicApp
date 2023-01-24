b8600b812461f1984104f8f6910eff3e3e7d13c6
Elizabeth <mwendemuthusi6@gmail.com>
2023-01-24 21:31:16 +0300

    Added the Library and SongItem structure

--- rhythmiq/src/component/Library.js
+++ rhythmiq/src/component/Library.js
@@ -3,7 +3,17 @@ import SongItem from './SongItem'
 
 const Library = ({songs, categories}) => {
   return (
-    <>Library</>
+    <div className='bg-secondary text-light text-center'>
+      <h3 className="py-3">Browse From our Collection</h3>
+      <div className="container ">
+        <div className="row g-3">
+
+          <SongItem/>
+
+
+          </div>
+      </div>
+    </div>
   )
 }
 
--- rhythmiq/src/component/SongItem.js
+++ rhythmiq/src/component/SongItem.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const SongItem = () => {
   return (
-    <div>SongItem</div>
+    <>
+    <div className="col-md">
+            <div className="card" style={{width: '10rem'}}>
+              <img className="card-img-top"
+                src="https://i.etsystatic.com/24578333/r/il/f84893/3538802227/il_794xN.3538802227_knii.jpg" alt="Card image cap"/>
+              <div className="card-body bg-dark">
+                <h5 className="card-title">Progressive</h5>
+              </div>
+            </div>
+          </div></>
   )
 }
 
