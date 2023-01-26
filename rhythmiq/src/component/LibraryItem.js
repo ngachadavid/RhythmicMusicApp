ac3b5e7c590898347069050a351526fb69f8cf11
Elizabeth <mwendemuthusi6@gmail.com>
2023-01-26 23:45:39 +0300

    Indented Library item data to look polished

--- rhythmiq/src/component/LibraryItem.js
+++ rhythmiq/src/component/LibraryItem.js
@@ -7,16 +7,16 @@ const LibraryItem = ({playlist, selectedPlaylist}) => {
   return (
     <>
     <div className="col col-lg-3 col-sm-6" onClick={handleClick}>
-            <div className="card" style={{width: '13rem', cursor:"pointer", height:'15%'}}>
-              
-              <div className="card-body bg-decided">
-                <i className="bi bi-music-note-beamed h1 text-warning"></i>
-                <h6 className="card-title py-5 text-warning">{playlist.name}</h6>
-                
-              </div>
-            </div>
-          </div>
-          </>
+      <div className="card" style={{width: '13rem', cursor:"pointer", height:'15%'}}>
+        
+        <div className="card-body bg-decided">
+          <i className="bi bi-music-note-beamed h1 text-warning"></i>
+          <h6 className="card-title py-5 text-warning">{playlist.name}</h6>
+          
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
