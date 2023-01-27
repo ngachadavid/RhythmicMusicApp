f536f7684311080d7667f361dd1518cda8d9ed1a
Elizabeth <mwendemuthusi6@gmail.com>
2023-01-27 03:32:52 +0300

    Added a static player

--- rhythmiq/src/component/Player.js
+++ rhythmiq/src/component/Player.js
@@ -1,14 +1,18 @@
 import React from 'react'
+// import ReactAudioPlayer from 'react-audio-player';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
-const Player = () => {
+const Player = ({songPath}) => {
+  const cool="../Music/WildVibes  Martin Miller ft Arild Aas  Far From You.mp3"
   return (
     <div className='elementp theme-bg p-5'>
-      <audio
-  
-  controls
-  src={require("../Music/Old Dominion-Written in the sand.mp3")}
-  type="audio/mpeg"
- />
+      <AudioPlayer
+    autoPlay
+    src={require("../Music/WildVibes  Martin Miller ft Arild Aas  Far From You.mp3")}
+    onPlay={e => console.log("onPlay")}
+    // other props here
+  />
       
       
     </div>
--- rhythmiq/src/component/PlaylistAdd.js
+++ rhythmiq/src/component/PlaylistAdd.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import Player from './Player'
+import {useNavigate} from "react-router-dom";
 
 function PlaylistAdd({song, onDisplay}) {
+  const navigator = useNavigate()
   function removeSong(){
     onDisplay(song.id)
   }
+  function handleClick(){
+    <Player songPath={song.mp3}/>
+    navigator('/player')
+
+  }
   
   return (
-    <tr>
+    <tr onClick={handleClick}>
     <td><img src={song.poster} class="img-fluid" style={{height:'3rem', width:'3rem'}}alt="" /></td>
 <td>{song.name}</td>
     <td>{song.artist}</td>
